e3eefb4335b3dbf0decaaf5238a5b94030998981
Brandon Garcia <grandonleebarcia@gmail.com>
2025-03-07 09:50:10 -0800

    added NavNab component

--- app/page.tsx
+++ app/page.tsx
@@ -1,68 +1,20 @@
 import DisplayPost from './components/DisplayPost';
 
-async function getData(sortBy: string, time: string) {
-  const url = `https://www.reddit.com/r/LivestreamFail/${sortBy}/.json?t=${time}`;
+import { findAllTwitchClips } from './utils/helpers';
+
+async function getData() {
+  const url = `https://www.reddit.com/r/LivestreamFail/.json`;
   const response = await fetch(url);
   const json = await response.json();
   return [...json.data.children];
 }
 
-function isFirstLetterCapital(str: string) {
-  if (!str) {
-    return false; // Handle empty strings
-  }
-  return /^[A-Z]/.test(str);
-}
-
-function isTwitchClip(str: string) {
-  return isFirstLetterCapital(str);
-}
-
-function trimClipID(str: string) {
-  return str.split('?')[0];
-}
-
-type Posts = {
-  data: {
-    url: string;
-    title: string;
-    ups: number;
-    downs: number;
-    thumbnail: string;
-  };
-  kind: string;
-};
-
-function findAllTwitchClips(posts: Posts[]) {
-  const twitchClips = [];
-
-  for (let i = 0; i < posts.length; i++) {
-    const lastElement = posts[i].data.url.split('/').length - 1;
-    const urlSplit = posts[i].data.url.split('/');
-    const currClipId = trimClipID(urlSplit[lastElement]);
-
-    if (isTwitchClip(currClipId)) {
-      console.log(i, posts[i].data.title);
-      const currClip = {
-        Id: currClipId,
-        title: posts[i].data.title,
-        ups: posts[i].data.ups,
-        downs: posts[i].data.downs,
-        thumbnail: posts[i].data.thumbnail,
-      };
-      twitchClips.push(currClip);
-    }
-  }
-
-  return [...twitchClips];
-}
-
 export default async function Home() {
-  const posts = await getData('hot', 'today');
+  const posts = await getData();
   const twitchClips = findAllTwitchClips(posts);
 
   return (
-    <div className="px-20 flex items-center justify-center min-h-screen">
+    <div className="px-20">
       <DisplayPost twitchClips={twitchClips} />
     </div>
   );
