b41cdb920d191cad97970ceb096dcee8c197b3a9
Brandon Garcia <grandonleebarcia@gmail.com>
2025-03-11 15:28:22 -0700

    add functionality: keep selected clip highlighted when click, only highlight other clips when user hovers

--- app/components/ListOfClips.tsx
+++ app/components/ListOfClips.tsx
@@ -1,4 +1,4 @@
-import React, { memo, Dispatch, SetStateAction } from 'react';
+import React, { memo, Dispatch, SetStateAction, useState } from 'react';
 
 import type { TwitchClips } from '../utils/types';
 
@@ -9,16 +9,24 @@ const ListOfClips = ({
   twitchClips: TwitchClips;
   setClipNum: Dispatch<SetStateAction<number>>;
 }) => {
-  const unselectedClip =
+  const highlightOnHover =
     'flex items-center py-2 hover:bg-gradient-to-r from-purple-500 to-purple-900 hover:text-white rounded-xl cursor-pointer';
+
+  const highlightOnClick =
+    'flex items-center py-2 bg-gradient-to-r from-purple-500 to-purple-900 text-white rounded-xl cursor-pointer';
+
+  const [selectedClip, setSelectedClip] = useState(0);
   return (
     <div className="h-[720px] pl-5 overflow-scroll overflow-x-hidden rounded-xl ">
-      {twitchClips.map((clip, i) => (
+      {twitchClips.map((clip, currClip) => (
         <div
-          className={unselectedClip}
-          key={i}
+          className={
+            selectedClip === currClip ? highlightOnClick : highlightOnHover
+          }
+          key={currClip}
           onClick={() => {
-            setClipNum(i);
+            setClipNum(currClip);
+            setSelectedClip(currClip);
           }}
         >
           <img
