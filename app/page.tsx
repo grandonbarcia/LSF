05960bb051e13233a72effeb80b56b63ef2e0e1d
Brandon Garcia <grandonleebarcia@gmail.com>
2025-05-21 14:53:42 -0700

    netlify doesn't like something, trying to fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,3 @@
-import DisplayPost from './components/DisplayPost';
-
-import { findAllTwitchClips } from './utils/helpers';
-
-async function getData() {
-  const url = `https://www.reddit.com/r/LivestreamFail/.json`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return [...json.data.children];
-}
-
 export default async function Home() {
-  const posts = await getData();
-  const twitchClips = findAllTwitchClips(posts);
-
-  return (
-    <div className="px-20">
-      <DisplayPost twitchClips={twitchClips} />
-    </div>
-  );
+  return <div className="px-20"></div>;
 }
